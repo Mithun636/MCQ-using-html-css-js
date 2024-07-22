62fa8098693a81db4b3ebe096f7aae1c788f181c
MITHUN R L <114131835+Mithun636@users.noreply.github.com>
2024-07-22 17:15:38 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function rt(){
+    document.getElementById('op4').style.backgroundColor="green";
+    document.getElementById('o4').style.backgroundColor="green";
+}
+function wrng1(){
+    document.getElementById('op1').style.backgroundColor="red";
+    document.getElementById('o1').style.backgroundColor="red";
+}
+function wrng2(){
+    document.getElementById('op2').style.backgroundColor="red";
+    document.getElementById('o2').style.backgroundColor="red";
+}
+function wrng3(){
+    document.getElementById('op3').style.backgroundColor="red";
+    document.getElementById('o3').style.backgroundColor="red";
+}
